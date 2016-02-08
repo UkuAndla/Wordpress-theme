dc5842873405eb67523c58b5b6d410af219bf8c1
ukuandla <ukuandla@gmail.com>
2016-02-08 21:08:08 +0200

    Lecture 58: Create a Custom Resources Template [deliver #113299535]

--- wp-admin/about.php
+++ wp-admin/about.php
@@ -169,7 +169,6 @@ $tech_features = array(
 			</div>
 
 
-
 			<div class="return-to-dashboard">
 				<?php if ( current_user_can( 'update_core' ) && isset( $_GET['updated'] ) ) : ?>
 					<a href="<?php echo esc_url( self_admin_url( 'update-core.php' ) ); ?>">
