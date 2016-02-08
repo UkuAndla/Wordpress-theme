27aaf81f2944b211df84b14957a32f01de49b6fd
ukuandla <ukuandla@gmail.com>
2016-02-08 21:17:43 +0200

    Lecture 68: Install Contact Form 7 Plugin and Add Your Custom Form [deliver #113300499]

--- wp-content/themes/bootstrap2wordpress/index.php
+++ wp-content/themes/bootstrap2wordpress/index.php
@@ -12,50 +12,49 @@
  */
 get_header(); ?>
 
-<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
-    <h1 class="page-title">Blog</h1>
-</section>
+    <section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+        <h1 class="page-title">Blog</h1>
+    </section>
 
+    <!-- BLOG CONTENT
+    ================================================== -->
+    <div class="container">
+        <div class="row" id="primary">
 
-<!-- BLOG CONTENT
-================================================== -->
-<div class="container">
-    <div class="row" id="primary">
+            <main id="content" class="col-sm-8" role="main">
 
-        <main id="content" class="col-sm-8" role="main">
+                <?php if ( have_posts() ) : ?>
 
-            <?php if ( have_posts() ) : ?>
+                    <?php /* Start the Loop */ ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php /* Start the Loop */ ?>
-                <?php while ( have_posts() ) : the_post(); ?>
+                        <?php
+                        /* Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        get_template_part( 'content', get_post_format() );
+                        ?>
 
-                    <?php
-                    /* Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    get_template_part( 'content', get_post_format() );
-                    ?>
+                    <?php endwhile; ?>
 
-                <?php endwhile; ?>
+                    <?php bootstrap2wordpress_paging_nav(); ?>
 
-                <?php bootstrap2wordpress_paging_nav(); ?>
+                <?php else : ?>
 
-            <?php else : ?>
+                    <?php get_template_part( 'content', 'none' ); ?>
 
-                <?php get_template_part( 'content', 'none' ); ?>
+                <?php endif; ?>
 
-            <?php endif; ?>
+            </main><!-- content -->
 
-        </main><!-- content -->
+            <!-- SIDEBAR
+            ================================================== -->
+            <aside class="col-sm-4">
+                <?php get_sidebar(); ?>
+            </aside>
 
-        <!-- SIDEBAR
-        ================================================== -->
-        <aside class="col-sm-4">
-            <?php get_sidebar(); ?>
-        </aside>
-
-    </div><!-- primary -->
-</div><!-- container -->
+        </div><!-- primary -->
+    </div><!-- container -->
 
 <?php get_footer(); ?>
